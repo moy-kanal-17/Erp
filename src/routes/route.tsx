a713cf8d42e85c03c8c21bd444ac0a139c6970a2
moy-kanal-17 <sevarahalilova28@gmail.com>
2025-07-30 08:33:14 +0500

    .

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -45,7 +45,9 @@ const Router = () => {
         {/* TEACHER LAYOUT */}
         <Route path="teacher" element={<LayoutProtect><TeacherPaneLayout/></LayoutProtect>}>
           <Route path="settings" element={<SettingsPage/>}/>
-          <Route index path="dashboard" element={<TeacherDashboard/>}/>
+          <Route index  element={<TeacherDashboard/>}/>
+          <Route path="dashboard" element={<TeacherDashboard/>}/>
+          
           <Route path="profile" element={<Profile></Profile>}/>
           <Route path="*" element={<NotFound/>}/>
 
